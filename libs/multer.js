b0cd80550735fcc98fb8f1322a94bbc7242f11d2
Rafijoee <rafijoe44@gmail.com>
2024-11-04 21:40:29 +0700

    feat : dinamis file directory

--- libs/multer.js
+++ libs/multer.js
@@ -1,21 +1,46 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './public/images'); // Folder public/images harus sudah ada
-    },
-    filename: (req, file, cb) => {
-        let newName = Date.now() + path.extname(file.originalname);
-        cb(null, newName);
+// Fungsi untuk memastikan folder tujuan ada
+const ensureDirExists = (dir) => {
+    if (!fs.existsSync(dir)){
+        fs.mkdirSync(dir, { recursive: true });
     }
-});
+};
+
+const storage = (destination) => {
+    const fullPath = path.join(__dirname, `../public/${destination}`);
+    ensureDirExists(fullPath);  // Pastikan folder tujuan ada
+    
+    return multer.diskStorage({
+        destination: (req, file, cb) => {
+            cb(null, fullPath); // Set direktori tujuan ke fullPath yang sudah dibuat
+        },
+        filename: (req, file, cb) => {
+            let newName = Date.now() + path.extname(file.originalname);
+            cb(null, newName);
+        }
+    });
+};
 
 module.exports = {
     image: multer({
-        storage: storage,
+        storage: storage("images"),
         fileFilter: (req, file, cb) => { 
             const allowedType = ["image/png", "image/jpeg"];  // Mengizinkan .png dan .jpg
+            if (allowedType.includes(file.mimetype)) {
+                cb(null, true);
+            } else {
+                cb(new Error("File type not supported"), false);
+            }
+        }
+    }),
+
+    videos: multer({
+        storage: storage("videos"),
+        fileFilter: (req, file, cb) => { 
+            const allowedType = ["video/mp4", "video/quicktime"];
             if (allowedType.includes(file.mimetype)) {
                 cb(null, true);
             } else {
